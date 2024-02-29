5add60063a2c4aaa0cf289c431e654ce272b43ba
laubrebr06 <laubrebr@gmail.com>
2024-02-29 10:56:24 -0600

    Realty Type Classic UI changes

--- DevClassic/Files/obj/Debug/DevClassic.AssemblyInfo.cs
+++ DevClassic/Files/obj/Debug/DevClassic.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DevClassic")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9374d4bb18aafafc63a9623e2dc4e2aae6454627")]
 [assembly: System.Reflection.AssemblyProductAttribute("DevClassic")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DevClassic")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
